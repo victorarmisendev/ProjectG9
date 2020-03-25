a68fe0efd16c9d2c8ed84fed2ec7fabc3176186e
victorarmisen <victorarmisencapo@enti.cat>
2020-03-25 12:53:17 +0100

    Begin Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject[] players;
+    private void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Base");
+    }
+    void Update()
+    {
+        Vector3 posFrame = new Vector3();
+        for (int i = 0; i < players.Length; i++)
+        {
+            posFrame += players[i].transform.position;
+        }
+        //Camera
+        transform.position = posFrame / players.Length;
+    }
+
+
+}
