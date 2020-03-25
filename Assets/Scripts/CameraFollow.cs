db17f6fd315d0aaae12f89361301c03ad4910796
victorarmisen <victorarmisencapo@enti.cat>
2020-03-25 18:32:55 +0100

    Camera movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,25 +7,27 @@ public class CameraFollow : MonoBehaviour
     private GameObject[] players;
     private Vector3 velocity;
     private float smoothTime = .5f;
+    public float speed;
     private void Start()
     {
-        players = GameObject.FindGameObjectsWithTag("Base");
+        //players = GameObject.FindGameObjectsWithTag("Base");
     }
     void LateUpdate()
     {
-        SetPos();
+        //SetPos();
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
     }
     void SetPos()
     {
-        Vector3 posFrame = Vector3.zero;
-        for (int i = 0; i < players.Length; i++)
-        {
-            posFrame += players[i].transform.position;
-        }
-        //Camera
-        posFrame /= players.Length;
-        Vector3 newPos = new Vector3(posFrame.x, 31, -24);
-        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+        //Vector3 posFrame = Vector3.zero;
+        //for (int i = 0; i < players.Length; i++)
+        //{
+        //    posFrame += players[i].transform.position;
+        //}
+        ////Camera
+        //posFrame /= players.Length;
+        //Vector3 newPos = new Vector3(posFrame.x, 31, -24);
+        //transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
     }
 
 
