f60c732b5efd29f1cef6c166f2512facf5a8401b
victorarmisen <victorarmisencapo@enti.cat>
2020-04-08 22:14:37 +0200

    correción de encaje de plataformas nuevas y codigo muerte lava en proceso

---
+++ Assets/Art/Dead.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Lava")
+        {
+            //Reset the position when the player collides lava. 
+
+            GameObject[] puentes = GameObject.FindGameObjectsWithTag("Puente");
+
+            //Get the last post before die. 
+
+            Vector3 position_player = transform.position;
+
+            //Conseguir un puente de mayor distancia en z que el player
+            //con un rango min and max. 
+
+            List<GameObject> PUENTES = new List<GameObject>();
+
+            foreach(var b in puentes)
+            {
+                if(b.transform.position.z > position_player.z)
+                {
+                    PUENTES.Add(b);
+                }
+            }
+
+            GameObject bridge = PUENTES[Random.Range(0, PUENTES.Capacity)];
+            Transform bTransform = bridge.transform;
+
+            gameObject.transform.position = new Vector3(bTransform.position.x, 2.0f, bTransform.position.z);
+        }
+
+    }
+
+}
---
+++ Assets/Art/GAMEMANGERPRUEBA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAMEMANGERPRUEBA : MonoBehaviour
+{
+
+
+     //Create and Spawn players in random position and assign pads. 
+        for (int i = 0; i<NumPlayers; i++)
+        {
+            GameObject player = (GameObject)Instantiate(playerTypes[Random.Range(0, playerTypes.Length)], PosCar(i), Quaternion.identity);
+
+            player.GetComponent<PlayerControler>().gamepad_current = pads[i];
+            player.GetComponent<PlayerControler>().PlayerNum = i + 1;
+            player.GetComponent<PlayerControler>().CameraKill = CameraKill;
+            player.GetComponent<PlayerControler>().respawnX = i;
+        }
+
+}
