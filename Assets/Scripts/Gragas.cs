c55a0c8a272e74e172be2509df9121b763f9d83d
Sergi G <s.gonzalezpons@gmail.com>
2020-04-25 12:02:39 +0200

    Gragas

---
+++ Assets/Scripts/Gragas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gragas : MonoBehaviour
+{
+
+    public int force;
+    public int minimumDistance;
+    GameObject[] players;
+    
+
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+    
+
+    void explode()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            Vector3 distance = gameObject.transform.position - players[i].transform.position;
+
+            if(distance.magnitude < minimumDistance)
+            {
+                players[i].GetComponent<Rigidbody>().AddForce(distance * force);
+            }
+        }
+    }
+}
