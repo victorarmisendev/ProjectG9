8816c9da1c7734857c89a2045ca26007acab4506
victorarmisen <victorarmisencapo@enti.cat>
2020-04-25 20:33:11 +0200

    tutorial in progress good results

--- Assets/Art/Art_Tutorial/Tutorial.cs
+++ Assets/Art/Art_Tutorial/Tutorial.cs
@@ -2,30 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    //public GameObject[] playerTypes;
-    private int NumPlayers;
-    public Gamepad pad;
-    private Gamepad [] allpads;
-    Gamepad mando;
-    
-    public GameObject player;
-    public Transform salida;
+    public GameObject[] chunks;
+    public float timer = 0.1f, originalTimer = 0.0f;
+    Vector3 currentChunkPos = Vector3.zero;
+    public Transform firstChunk;
+    public Vector3 offsetZ;
+    private int first = 0;
+    private float normal_spawn = 0.0f;
+    private int fase = 0;
+
+    public string[] helperTexts;
+    public Text helper;
+
+    float timerPhase = 10.0f;
+
+    private GameObject chunkToSpawn;
 
     private void Start()
     {
-        NumPlayers = Gamepad.all.Count;
-        allpads = Gamepad.all.ToArray();
-        pad = allpads[0];
+        //Camera speed and timer. 
+        normal_spawn = 40.0f / Camera.main.gameObject.GetComponent<CameraFollow>().speed; //SPAWNER
+        //print(timer);
+        originalTimer = timer;
+
+        //TUTORIAL:
+        currentChunkPos = firstChunk.position;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            //Spawn a random chunk. 
+            InvokeChunk(offsetZ, fase); //All the chunks are the same size. In z. 
+            timer = originalTimer;
+            first++;
+        }
+        if (first >= 2)
+        {
+            originalTimer = normal_spawn;
+        }
+
+        //TIMER FASE CHANGE: EVERY MIN.
+        timerPhase -= Time.deltaTime;
+        if(timerPhase <= 0.0f && fase < 4)
+        {
+            timerPhase = 10.0f;
+            fase++;
+        }
+        //FEEDBACK TUTORIAL IN THESE MINUTES: 
+        //TEXT! 
+        helper.text = helperTexts[fase];
+        Debug.Log(fase);
+    }
 
 
-        GameObject p = Instantiate(player, salida.position, player.transform.rotation);
-
-        /* p.GetComponent<PlayerControler>().PlayerNum = 1;*/ //NONE
-        //p.GetComponent<PlayerControler>().name = "Player" + p.GetComponent<PlayerControler>().PlayerNum;
-        //p.GetComponent<PlayerControler>().gamepad_current = pad;
-        
+    void InvokeChunk(Vector3 offset, int phase) //INVOKE THE CHUNKS.
+    {
+        Vector3 newPos = currentChunkPos + offset;
+        GameObject c = Instantiate(chunks[phase], newPos, chunks[fase].transform.rotation);
+        //Destroy(c, 32.0f); //Los elimina antes idk, DANGER.
+        currentChunkPos = newPos;
     }
 }
