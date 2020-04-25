82c2a0b1bdb1cd9e7e3b714f390e3358b2d939ef
victorarmisen <victorarmisencapo@enti.cat>
2020-04-25 20:02:40 +0200

    begin onboarding

---
+++ Assets/Art/Art_Tutorial/Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Tutorial : MonoBehaviour
+{
+    //public GameObject[] playerTypes;
+    private int NumPlayers;
+    public Gamepad pad;
+    private Gamepad [] allpads;
+    Gamepad mando;
+    
+    public GameObject player;
+    public Transform salida;
+
+    private void Start()
+    {
+        NumPlayers = Gamepad.all.Count;
+        allpads = Gamepad.all.ToArray();
+        pad = allpads[0];
+
+
+        GameObject p = Instantiate(player, salida.position, player.transform.rotation);
+
+        /* p.GetComponent<PlayerControler>().PlayerNum = 1;*/ //NONE
+        //p.GetComponent<PlayerControler>().name = "Player" + p.GetComponent<PlayerControler>().PlayerNum;
+        //p.GetComponent<PlayerControler>().gamepad_current = pad;
+        
+    }
+}
