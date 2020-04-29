a8debe78af3bea5b89b1abc7ca81c55f95015e31
Ramon Mormeneo <ramonmormeneoferrer@gmail.com>
2020-04-29 19:31:41 +0200

    sound asset + select

--- Assets/Art/AuxScripts/CharacterSelection/CharacterSelection.cs
+++ Assets/Art/AuxScripts/CharacterSelection/CharacterSelection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelection : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class CharacterSelection : MonoBehaviour
     //Activar sets de escoger personaje: 
     public GameObject SelectorPrefab;
     public Transform[] positions;
-
+    public List<int> Lista;
     void Start()
     {
         //conseguir players. NUM de jugadores. 
@@ -24,10 +25,15 @@ public class CharacterSelection : MonoBehaviour
             //Instance the Selectors: 
             GameObject selector = Instantiate(SelectorPrefab, positions[i].position, Quaternion.identity);
             selector.GetComponent<Selector>().currentPad = pads[i];
+            Lista.Add(selector.GetComponent<Selector>().state);
         }
-
+        DontDestroyOnLoad(transform.gameObject);
     }
-
-    
-
+    private void Update()
+    {
+        if (pads[0].aButton.isPressed)
+        {
+            SceneManager.LoadScene("PruebasArte");
+        }
+    }
 }
