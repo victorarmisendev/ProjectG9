c1acde1e87b40c657f3804508244198265f6514b
victorarmisen <victorarmisencapo@enti.cat>
2020-05-18 20:32:20 +0200

    Menu global working ( early stages )

---
+++ Assets/Scripts/Menus/CameraMovementMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementMenu : MonoBehaviour
+{
+
+    public GameObject menuGlobal;
+    private MenuGlobal menu_;
+    private float [] sectionsPos; //Altura de movimiento de las secciones. 
+
+    void Start()
+    {
+
+        menu_ = menuGlobal.GetComponent<MenuGlobal>();
+
+        sectionsPos = new float[3];
+        sectionsPos[0] = menu_.sections[0].transform.position.y + 8;
+        sectionsPos[1] = menu_.sections[1].transform.position.y;
+        sectionsPos[2] = menu_.sections[2].transform.position.y;
+    }
+
+    void Update()
+    {
+        
+        if(menu_.state == 1)
+        {
+            //Mover hasta punto indicado. 
+            float step = 1.5f * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, sectionsPos[0], transform.position.z), step);
+        }
+        if (menu_.state == 2)
+        {
+            float step = 1.5f * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, sectionsPos[1], transform.position.z), step);
+        }
+        if (menu_.state == 3)
+        {
+            float step = 1.5f * Time.deltaTime; // calculate distance to move
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, sectionsPos[2], transform.position.z), step);
+        }
+    }
+
+   
+
+}
---
+++ Assets/Scripts/Menus/MenuGlobal.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuGlobal : MonoBehaviour
+{
+    public GameObject[] sections;
+    public int state = 1;
+    public Gamepad pad;
+
+    private void Start()
+    {
+        pad = Gamepad.all[0]; //El primer jugador lleva el menu.
+
+        //Reset
+        foreach(var s in sections)
+        {
+            s.SetActive(false);
+        }
+
+        sections[0].SetActive(true);
+
+    }
+
+    void Update()
+    {
+        state = Mathf.Clamp(state, 1, 3); // Min y max por las secciones del menu global. 
+
+        InputMovement();
+
+        ChangeSections(state);
+
+    }
+
+    void InputMovement()
+    {
+        if (pad.aButton.wasPressedThisFrame)
+        {
+            //Cambio de section. 
+            state++;
+        }
+
+        if (pad.bButton.wasPressedThisFrame)
+        {
+            //Cambio de section. 
+            state--;
+        }
+
+    }
+
+    void ChangeSections(int state)
+    {
+
+        switch(state)
+        {
+            case 1:
+                //Active section one. 
+                sections[0].SetActive(true);
+                sections[1].SetActive(false);
+                sections[2].SetActive(false);
+                break;
+            case 2:
+                sections[0].SetActive(false);
+                sections[1].SetActive(true);
+                sections[2].SetActive(false);
+                break;
+            case 3:
+                sections[0].SetActive(false);
+                sections[1].SetActive(false);
+                sections[2].SetActive(true);
+                break;
+            default: //Nothing for now.
+                break;
+        }
+    }
+
+
+
+}
