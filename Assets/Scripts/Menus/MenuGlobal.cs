ebcb84707469616ecdb20884b063a21b35f0db6c
victorarmisen <victorarmisencapo@enti.cat>
2020-05-24 22:51:21 +0200

    menu logic almost working

--- Assets/Scripts/Menus/MenuGlobal.cs
+++ Assets/Scripts/Menus/MenuGlobal.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MenuGlobal : MonoBehaviour
 {
@@ -11,18 +12,19 @@ public class MenuGlobal : MonoBehaviour
     public Gamepad pad;
     public bool PRESSED_A = false;
     public GameObject cam;
+    public Text text, text2;
+    private int aux = 3;
 
-    bool isMoving = false;
 
     private void Start()
     {
         pad = Gamepad.all[0]; //El primer jugador lleva el menu.
 
         //Reset
-        foreach(var s in sections)
-        {
-            s.SetActive(false);
-        }
+        //foreach(var s in sections)
+        //{
+        //    s.SetActive(false);
+        //}
 
         sections[0].SetActive(true);
 
@@ -39,14 +41,16 @@ public class MenuGlobal : MonoBehaviour
 
         MainMenu();
 
-        Debug.Log("The state menu contador is: " + stateMenu);
+        //Debug.Log("The state menu contador is: " + stateMenu);
         //Movements: 
+
+        text.text = "The state menu contador is: " + stateMenu.ToString();
 
     }
 
     void InputMovement()
     {
-        if (stateMenu <= 2)
+        if (stateMenu < 2)
         {
             if (pad.aButton.wasPressedThisFrame)
             {
@@ -54,33 +58,58 @@ public class MenuGlobal : MonoBehaviour
                 stateMenu++;
             }
 
-        }
-
-        if (stateMenu <= 3)
-        {           
             if (pad.bButton.wasPressedThisFrame)
             {
                 //Cambio de section. 
                 stateMenu--;
             }
+
         }
+        else
+        {
+            if (pad.bButton.wasPressedThisFrame)
+            {
+                //Cambio de section. 
+                stateMenu = 2;
+            }
+        }
+
+        //if (stateMenu <= 3)
+        //{           
+        //    if (pad.bButton.wasPressedThisFrame)
+        //    {
+        //        //Cambio de section. 
+        //        stateMenu--;
+        //    }
+        //}
       
     }
 
     void JoyStickMainMenu()
     {
-        if (stateMenu > 2)
+
+        aux = Mathf.Clamp(aux, 3, 7);
+
+        if (stateMenu == 2)
         {
+            text2.text = "The menu contador is: " + aux.ToString();
             if (pad.leftStick.up.wasPressedThisFrame)
             {
                 //Cambio de section. 
-                stateMenu++;
+                aux++;
+                Debug.Log("HA ENTRADO JOYSTICK IZQUIERDO");
             }
 
             if (pad.leftStick.down.wasPressedThisFrame)
             {
                 //Cambio de section. 
-                stateMenu--;
+                aux--;
+                Debug.Log("HA ENTRADO JOYSTICK IZQUIERDO");
+            }
+            if (pad.aButton.wasPressedThisFrame)
+            {
+                stateMenu = aux; //Confirmacion de a donde vamos.
+                aux = 3;
             }
         }
     }
@@ -116,32 +145,63 @@ public class MenuGlobal : MonoBehaviour
         {
             float step = 1.5f * Time.deltaTime;
             Vector3 camera_pos = cam.transform.position;
+
             cam.transform.position = Vector3.Lerp(cam.transform.position,
-                new Vector3(camera_pos.x, sections[0].transform.position.y, camera_pos.z), step);
+                new Vector3(sections[0].transform.position.x, sections[0].transform.position.y, camera_pos.z), step);
         }
 
         if (state == 2)
         {
             float step = 1.5f * Time.deltaTime;
             Vector3 camera_pos = cam.transform.position;
+
             cam.transform.position = Vector3.Lerp(cam.transform.position,
-                new Vector3(camera_pos.x, sections[1].transform.position.y, camera_pos.z), step);
+                new Vector3(sections[1].transform.position.x, sections[1].transform.position.y, camera_pos.z), step);
         }
 
         if (state == 3)
         {
             float step = 1.5f * Time.deltaTime;
             Vector3 camera_pos = cam.transform.position;
+
             cam.transform.position = Vector3.Lerp(cam.transform.position,
                 new Vector3(camera_pos.x, sections[2].transform.position.y, camera_pos.z), step);
         }
 
         if (state == 4)
         {
-            //float step = 1.5f * Time.deltaTime;
-            //Vector3 camera_pos = cam.transform.position;
-            //cam.transform.position = Vector3.Lerp(cam.transform.position,
-            //    new Vector3(camera_pos.x, sections[0].transform.position.y, camera_pos.z), step);
+            float step = 1.5f * Time.deltaTime;
+            Vector3 camera_pos = cam.transform.position;
+
+            cam.transform.position = Vector3.Lerp(cam.transform.position,
+                new Vector3(sections[3].transform.position.x, sections[3].transform.position.y, camera_pos.z), step);
+        }
+
+        if (state == 5)
+        {
+            float step = 1.5f * Time.deltaTime;
+            Vector3 camera_pos = cam.transform.position;
+
+            cam.transform.position = Vector3.Lerp(cam.transform.position,
+                new Vector3(sections[4].transform.position.x, sections[4].transform.position.y, camera_pos.z), step);
+        }
+
+        if (state == 6)
+        {
+            float step = 1.5f * Time.deltaTime;
+            Vector3 camera_pos = cam.transform.position;
+
+            cam.transform.position = Vector3.Lerp(cam.transform.position,
+                new Vector3(sections[5].transform.position.x, sections[5].transform.position.y, camera_pos.z), step);
+        }
+
+        if (state == 7)
+        {
+            float step = 1.5f * Time.deltaTime;
+            Vector3 camera_pos = cam.transform.position;
+
+            cam.transform.position = Vector3.Lerp(cam.transform.position,
+                new Vector3(sections[6].transform.position.x, sections[6].transform.position.y, camera_pos.z), step);
         }
 
     }
