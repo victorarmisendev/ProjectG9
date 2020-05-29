7bce4a183ff75338b8d6429680444557ec97befc
Victor Armisen Capo <victor.armisen.developer@gmail.com>
2020-05-29 21:39:26 +0200

    escene new infinite working better

---
+++ Assets/Scripts/New Infinite/CameraPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public static bool start = false;
+
+    private IEnumerator coroutine;
+    void Start()
+    {
+        coroutine = ExecuteAfterTime();
+        StartCoroutine(coroutine);
+    }
+
+    void FixedUpdate()
+    {
+        //transform.position = 
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+    }
+
+    IEnumerator ExecuteAfterTime()
+    {
+        yield return new WaitForSeconds(3);
+        start = true;
+        // Code to execute after the delay
+    }
+
+
+}
--- Assets/Scripts/New Infinite/PlayerRails.cs
+++ Assets/Scripts/New Infinite/PlayerRails.cs
@@ -11,7 +11,13 @@ public class PlayerRails : MonoBehaviour
     private int count = 0;
     private Rigidbody rb;
     public int speed; //Constant speed player
-    public float timerChangeRail;
+    Vector3 toPos;
+    public int speedChangeRail;
+    Quaternion initialRot;
+    public GameObject partOfTheCar;
+    public int lives;
+    public Camera main;
+    GameObject toSpawn = null;
 
     void Start()
     {
@@ -20,22 +26,21 @@ public class PlayerRails : MonoBehaviour
         pad = Gamepad.all[0];
         transform.position = rails[0].transform.position;
         rb = GetComponent<Rigidbody>();
+        initialRot = rb.rotation;
 
     }
 
     private void FixedUpdate()
     {
-        //if (CameraFollow.start)
-        //{
-        //    rb.MovePosition(rb.position + Vector3.forward * speed * Time.fixedDeltaTime);
-
-        //    //LERP ON: 
-            
-
-        //}
-
-
-
+        if (CameraFollow.start)
+        {
+            toPos = new Vector3(rails[count].transform.position.x,
+                        transform.position.y,
+                        transform.position.z);
+            rb.position = Vector3.Lerp(transform.position, toPos, Time.fixedDeltaTime * speedChangeRail);
+            rb.rotation = Quaternion.Lerp(rb.rotation, initialRot, Time.fixedDeltaTime * speedChangeRail);
+            rb.MovePosition(rb.position + (Vector3.forward * speed) * Time.fixedDeltaTime);            
+        }
     }
 
     void Update()
@@ -48,11 +53,6 @@ public class PlayerRails : MonoBehaviour
                 //Go to rail of his left. 
                 count--;
                 count = Mathf.Clamp(count, 0, rails.Length - 1);
-                //transform.position = rails[count].transform.position;
-                //transform.position = new Vector3(rails[count].transform.position.x,
-                //                                transform.position.y,
-                //                                transform.position.z);
-                timerChangeRail = 0.0f;
             }
 
             if (pad.leftStick.right.wasPressedThisFrame)
@@ -60,28 +60,35 @@ public class PlayerRails : MonoBehaviour
                 //Go to rail of his right. 
                 count++;
                 count = Mathf.Clamp(count, 0, rails.Length - 1);
-                //transform.position = new Vector3(rails[count].transform.position.x,
-                //                    transform.position.y,
-                //                    transform.position.z);
-                timerChangeRail = 0.0f;
             }
-            if (timerChangeRail <= 0.0f)
-                timerChangeRail += 0.1f;
 
-            Vector3 toPos = new Vector3(rails[count].transform.position.x,
-                                    transform.position.y,
-                                    transform.position.z);
-
-            timerChangeRail = Mathf.Clamp(timerChangeRail, 0, 1.0f);
-            transform.position = Vector3.Lerp(transform.position, toPos, timerChangeRail);
-            transform.position += Vector3.forward * speed * Time.deltaTime;
-
-        }
-       
-
-       
-
+            //speed
+            RaycastHit hit;
+            if (Physics.Raycast(main.transform.position,
+                main.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.transform.gameObject.tag == "field")
+                {
+                    var boneChild = hit.transform.Find("Reset");
+                    toSpawn = boneChild.gameObject;
+                    Debug.Log("Enter" + " :" + hit.transform.name);
+                }
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            }
 
 
+            if (partOfTheCar.GetComponent<Renderer>().isVisible == false)
+            {
+                lives--;
+                //Change pos. Change pos in a place without collision or walls. 
+                //Get a visibles fields seen in the camera.
+                transform.position = toSpawn.transform.position;
+
+            }
+
+        }    
     }
+
+
+  
 }
