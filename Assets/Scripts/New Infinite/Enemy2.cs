32492b32893666b74f457117a70864c9517dfc44
Victor Armisen Capo <victor.armisen.developer@gmail.com>
2020-05-30 18:59:31 +0200

    Enemigo 2 working

--- Assets/Scripts/New Infinite/Enemy2.cs
+++ Assets/Scripts/New Infinite/Enemy2.cs
@@ -7,7 +7,11 @@ public class Enemy2 : MonoBehaviour
     private bool active = false;
     public Rigidbody rb;
     public GameObject explosion;
-    public float speed;
+    public float speed, speedFrenquency;
+    public float coef;
+    private float count = 0;
+
+    public float amplitud = 10.0f, frenquency = 0.7f;
 
     void Start()
     {
@@ -18,9 +22,39 @@ public class Enemy2 : MonoBehaviour
     {
         if (active)
         {
-            rb.MovePosition(rb.position - Vector3.forward * speed * Time.fixedDeltaTime);
-            rb.MovePosition(rb.position + Vector3.up *
-                Mathf.Sin(Time.fixedDeltaTime * speed));
+            //rb.MovePosition(rb.position - Vector3.forward * speed * Time.fixedDeltaTime);
+            
+            //rb.MovePosition(rb.position + 
+            //    Vector3.up *
+            //   (coef * Mathf.Abs(Mathf.Sin(Time.time * speedFrenquency))));
+            //print(count);
+        }
+    }
+
+    private void Update()
+    {
+        if (active)
+        {
+            float x = transform.position.x;
+            float y = 10.0f + Mathf.Sin(Time.time * frenquency) * amplitud;
+
+            transform.position -= (Vector3.forward * speed) * Time.deltaTime;
+            float z = transform.position.z;
+            transform.position = new Vector3(x, y, z);
+
+            transform.position += Vector3.up * 6.0f * Mathf.Sin(Time.time * 3f) * 1f;
+        }
+        //print(Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerRails>().lives--;
+            GameObject par = Instantiate(explosion, rb.position, explosion.transform.rotation);
+            Destroy(par, 3.0f);
+            Destroy(gameObject, 0.5f);
         }
     }
 
