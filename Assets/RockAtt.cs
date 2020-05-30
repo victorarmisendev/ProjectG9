620ad8b0d55f2ac33f358d5964eff5385bd8d5f0
Sergi G <s.gonzalezpons@gmail.com>
2020-05-31 01:17:21 +0200

    Bastantes cosas arena

---
+++ Assets/RockAtt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockAtt : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PlayerArena")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
