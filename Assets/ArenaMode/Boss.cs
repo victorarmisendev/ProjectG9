49f759e051f11d7c29c02b776c01cfd7ad13ed45
Sergi G <s.gonzalezpons@gmail.com>
2020-05-31 14:02:36 +0200

    new boss attack and better logic

---
+++ Assets/ArenaMode/Boss.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public GameObject rockFly;
+    public GameObject rockFloor;
+    public GameObject player;
+    private Vector3 playerPos;
+
+    public int attackCD = 4;
+
+    private int boulderCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerArena");
+        playerPos = player.transform.position;
+        //InvokeRepeating("attack", attackCD, attackCD);
+        attack();
+    }
+
+    void attack()
+    {
+        playerPos = player.transform.position;
+        int i = Random.Range(1, 2);
+        switch(i)
+        {
+            case 0:
+                rockFlyAttack();
+                break;
+            case 1:
+                boulderCount = 0;
+                while (boulderCount < 3)
+                {
+                    InvokeRepeating("rockRollAttack", 1, 1);
+                }
+                rockRollAttack();
+                break;
+            case 2:
+                spawnMinion();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void rockFlyAttack()
+    {
+        Instantiate(rockFly, playerPos, new Quaternion(0, 0, 0, 1)); ;
+    }
+    void spawnMinion()
+    {
+
+    }
+    void rockRollAttack()
+    {
+        boulderCount++;
+        float xPos = Random.Range(-22.0f, 23.75f);
+        Instantiate(rockFloor, new Vector3 (xPos, 15, 12.5f), new Quaternion(0, 0, 0, 1));
+
+    }
+}
--- Assets/ArenaMode/RockAtt.cs
+++ Assets/ArenaMode/RockAtt.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class RockAtt : MonoBehaviour
 {
+    public GameObject[] elements1;
+    public GameObject particles;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "PlayerArena")
         {
             Destroy(other.gameObject);
+            for (int i = 0; i < elements1.Length; i++)
+            {
+                Destroy(elements1[i]);
+            }
         }
+        else if(other.gameObject.tag == "field")
+        {
+            for (int i = 0; i < elements1.Length; i++)
+            {
+                Destroy(elements1[i]);
+            }
+        }
+        particles.SetActive(true);
+
     }
 }
