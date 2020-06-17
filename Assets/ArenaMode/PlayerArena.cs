a5483425fca36f06c0a5067b246faa68228407c4
Sergi G <s.gonzalezpons@gmail.com>
2020-06-18 01:18:14 +0200

    almost finished

--- Assets/ArenaMode/PlayerArena.cs
+++ Assets/ArenaMode/PlayerArena.cs
@@ -15,6 +15,8 @@ public class PlayerArena : MonoBehaviour
     public AudioSource engineSound;
     public float audioVolume;
 
+    public int lives = 3;
+
     // Start is called before the first frame update
     void Start()
     {
