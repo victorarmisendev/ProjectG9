8c8e7d5c257819ce01add7ec96ce6bb978162f34
Sergi G <s.gonzalezpons@gmail.com>
2020-06-21 18:30:04 +0200

    Old chunks destroy

--- Assets/Scripts/New Infinite/Procedural.cs
+++ Assets/Scripts/New Infinite/Procedural.cs
@@ -8,11 +8,20 @@ public class Procedural : MonoBehaviour
     public GameObject[] fields;
     private Vector3 newPos = Vector3.zero;
     public GameObject lastField;
-    
+    public float timeSpawn = 1.5f;
+
+    int counter = 1;
+    Queue<GameObject> myQueue = new Queue<GameObject>();
+
     void Start()
     {
+        myQueue.Enqueue(lastField);
         newPos = lastField.transform.position;
-        InvokeRepeating("Instance", 0.5f, 1.1f);
+        InvokeRepeating("Instance", 0.5f, timeSpawn);
+        Instance();
+        Instance();
+        Instance();
+        Instance();
     }
 
     GameObject RandomFunction(GameObject[] fields)
@@ -25,9 +34,24 @@ public class Procedural : MonoBehaviour
         newPos = lastField.transform.position + Vector3.forward * 83.1f;
         GameObject fieldInstanced = Instantiate(RandomFunction(fields), newPos, Quaternion.identity);
         lastField = fieldInstanced;
+        myQueue.Enqueue(lastField);
+        counter++;
+
+        if(counter >= 11)
+        {
+            removeChunk();
+        }
+
         //Destroy(fieldInstanced, 10.0f);
         //Borrar todas aquellas fields que ya se han creado y no se vean en pantalla y que esten por detras
         //del jugador. 
+    }
+    void removeChunk()
+    {
+        GameObject chunkToDestroy;
+        chunkToDestroy = myQueue.Dequeue();
+        Destroy(chunkToDestroy);
+        counter--;
     }
 
 }
