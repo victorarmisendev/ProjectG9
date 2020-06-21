89cb916dd71b4819b41ee9ac62e00322a5267f3b
Victor Armisen Capo <victor.armisen.developer@gmail.com>
2020-06-21 23:41:39 +0200

    tutorial

---
+++ Assets/Scenes/Modos de juego/TutorialRunner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+public class TutorialRunner : MonoBehaviour
+{
+    public Gamepad pad;
+    bool ARENA = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
