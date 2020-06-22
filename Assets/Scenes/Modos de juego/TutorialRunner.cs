b9eb6f0311d3aaedb417fed008b31209db4ca344
Victor Armisen Capo <victor.armisen.developer@gmail.com>
2020-06-22 04:53:09 +0200

    tutorial better but with the same bug car moving

--- Assets/Scenes/Modos de juego/TutorialRunner.cs
+++ Assets/Scenes/Modos de juego/TutorialRunner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class TutorialRunner : MonoBehaviour
 {
     public Gamepad pad;
@@ -27,7 +28,8 @@ public class TutorialRunner : MonoBehaviour
     public bool shield = false;
     public int score = 0;
     public GameObject altura;
-
+    public TextMeshProUGUI mode;
+    public GameObject infinite, arena;
 
     void Start()
     {
@@ -41,46 +43,46 @@ public class TutorialRunner : MonoBehaviour
     {
         if(ARENA)
         {
-            if (pad.rightTrigger.isPressed)
-            {
-                if (speed < 0.8f)
-                {
-                    speed += 0.003f;
-                }
-                else if (speed >= 0.08f)
-                {
-                    speed = 0.8f;
-                }
+            //if (pad.rightTrigger.isPressed)
+            //{
+            //    if (speed < 0.8f)
+            //    {
+            //        speed += 0.003f;
+            //    }
+            //    else if (speed >= 0.08f)
+            //    {
+            //        speed = 0.8f;
+            //    }
 
-            }
-            else if (pad.leftTrigger.isPressed)
-            {
-                print("left");
-                if (speed > -0.3f)
-                {
-                    speed -= 0.003f;
-                }
-                else if (speed <= -0.3f)
-                {
-                    speed = -0.3f;
-                }
-            }
-            else if (!pad.rightTrigger.isPressed)
-            {
+            //}
+            //else if (pad.leftTrigger.isPressed)
+            //{
+            //    print("left");
+            //    if (speed > -0.3f)
+            //    {
+            //        speed -= 0.003f;
+            //    }
+            //    else if (speed <= -0.3f)
+            //    {
+            //        speed = -0.3f;
+            //    }
+            //}
+            //else if (!pad.rightTrigger.isPressed)
+            //{
 
-                if (!pad.leftTrigger.isPressed)
-                {
-                    if (speed > 0.0f)
-                    {
-                        speed -= 0.005f;
-                    }
-                    else
-                    {
-                        speed = 0.0f;
-                    }
-                }
+            //    if (!pad.leftTrigger.isPressed)
+            //    {
+            //        if (speed > 0.0f)
+            //        {
+            //            speed -= 0.005f;
+            //        }
+            //        else
+            //        {
+            //            speed = 0.0f;
+            //        }
+            //    }
 
-            }
+            //}
             //if (speed > 0.003f)
             //{
             //    engineSound.volume = speed;
@@ -119,8 +121,16 @@ public class TutorialRunner : MonoBehaviour
             //}
 
             //transform.Rotate(0.0f, direction, 0.0f);
+            if (pad.rightTrigger.isPressed)
+            {
+                rb.MovePosition(rb.position + transform.forward * 3.0f);
+            }
 
-            rb.MovePosition(rb.position + transform.forward * speed);
+            if (pad.leftTrigger.isPressed)
+            {
+                rb.MovePosition(rb.position + transform.forward * -3.0f);
+            }
+
             Quaternion deltaRot = Quaternion.Euler(0.0f, direction, 0.0f);
             rb.MoveRotation(rb.rotation * deltaRot);
         } 
@@ -143,10 +153,15 @@ public class TutorialRunner : MonoBehaviour
         }
         if(ARENA)
         {
-
+            mode.text = "Arena";
+            infinite.SetActive(false);
+            arena.SetActive(true);
         } 
         else
         {
+            mode.text = "Infinite";
+            infinite.SetActive(true);
+            arena.SetActive(false);
             if (pad.leftStick.left.wasPressedThisFrame)
             {
                 //Go to rail of his left. 
